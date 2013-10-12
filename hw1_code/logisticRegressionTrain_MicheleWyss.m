5a7ac35b726f6e7982b317d13682dd5003568a96
Michele <michele_wyss@students.unibe.ch>
2013-10-12 16:44:52 +0200

    removed some zombie stuff and made more comments

--- hw1_code/logisticRegressionTrain_MicheleWyss.m
+++ hw1_code/logisticRegressionTrain_MicheleWyss.m
@@ -1,20 +1,18 @@
 function [ theta ] = logisticRegressionTrain_MicheleWyss( DataTrain, LabelsTrain, maxIterations )
-% logisticRegressionTrain train a logistic regression classifier
-% [ theta ] = logisticRegressionTrain( DataTrain, LabelsTrain, MaxIterations )
 % Using the training data in DataTrain and LabelsTrain trains a logistic
 % regression classifier theta. 
 % 
 % Implement a Newton-Raphson algorithm.
-
+%
 % DataTrain = 
 % x1.1 x1.2 x1.3 x1.4 ... x1.576
 % ...
 % xm.1 xm.2 x2.3 ...  ... xm.576
-
+%
 % LabelsTrain = 
 % (y1 y2 y3 y4 ... ym)
-%
-%
+
+%% Initializations
 
 % map labels to [0,1]
 LabelsTrain = (LabelsTrain > 0);
@@ -34,28 +32,26 @@ sigmoid = @(x) 1/(1 + exp(-x));
 % theta and x have to be column vectors.
 h = @(theta_vec,x) sigmoid(theta_vec'*x);
 
-%grad_log_likelihood = @(theta) 1/m * sum((LabelsTrain(1,:) - h(theta,DataTrain(:,:)')) * DataTrain(:,:));
-
-%hessian = @(theta_vec) (-1/m) * sum(h(theta_vec,DataTrain(:,:)') .* (1 - h(theta_vec,DataTrain(:,:)')) .* DataTrain(:,:)' * DataTrain(:,:));
-
+%% Optimization
 for i=1:maxIterations
 
-    % compute gradient of the log likelihood
+    %% compute gradient of the log likelihood
     grad_log_likelihood = zeros(1,dimension);
     for k=1:m
-        grad_log_likelihood = grad_log_likelihood + (LabelsTrain(1,k) - h(theta,DataTrain(k,:)')) * DataTrain(k,:);
+        grad_log_likelihood = grad_log_likelihood + (LabelsTrain(k) - h(theta,DataTrain(k,:)')) * DataTrain(k,:);
     end
     
     grad_log_likelihood = grad_log_likelihood/m;
     
     
-    % compute hessian
+    %% compute hessian
     hessian = zeros(dimension);
     for k=1:m
         hessian = hessian + (h(theta,DataTrain(k,:)') .* (1 - h(theta,DataTrain(k,:)')) .* DataTrain(k,:)' * DataTrain(k,:));
     end
     
     hessian = -hessian ./m;
-
+    
+    %% update theta (Newton-Raphson)
     theta = theta - inv(hessian) * grad_log_likelihood';
 end
