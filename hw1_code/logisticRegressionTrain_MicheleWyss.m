1bd65e0ac2abe8142db3cc1c5b92e68cf84c071b
Michele <michele_wyss@students.unibe.ch>
2013-10-12 16:25:43 +0200

    not anymore storing all the iterations in theta

--- hw1_code/logisticRegressionTrain_MicheleWyss.m
+++ hw1_code/logisticRegressionTrain_MicheleWyss.m
@@ -22,7 +22,7 @@ LabelsTrain = (LabelsTrain > 0);
 % number of training samples
 m = size(LabelsTrain,2);
 
-theta = zeros(577,maxIterations);
+theta = zeros(577,1);
 
 % sigmoid function
 sigmoid = @(x) 1/(1 + exp(-x));
@@ -35,12 +35,12 @@ h = @(theta_vec,x) sigmoid(theta_vec'*x);
 
 %hessian = @(theta_vec) (-1/m) * sum(h(theta_vec,DataTrain(:,:)') .* (1 - h(theta_vec,DataTrain(:,:)')) .* DataTrain(:,:)' * DataTrain(:,:));
 
-for i=2:maxIterations
+for i=1:maxIterations
 
     % compute gradient of the log likelihood
     grad_log_likelihood = zeros(1,577);
     for k=1:m
-        grad_log_likelihood = grad_log_likelihood + (LabelsTrain(1,k) - h(theta(:,i-1),DataTrain(k,:)')) * DataTrain(k,:);
+        grad_log_likelihood = grad_log_likelihood + (LabelsTrain(1,k) - h(theta,DataTrain(k,:)')) * DataTrain(k,:);
     end
     
     grad_log_likelihood = grad_log_likelihood/m;
@@ -49,12 +49,10 @@ for i=2:maxIterations
     % compute hessian
     hessian = zeros(577);
     for k=1:m
-        hessian = hessian + (h(theta(:,i-1),DataTrain(k,:)') .* (1 - h(theta(:,i-1),DataTrain(k,:)')) .* DataTrain(k,:)' * DataTrain(k,:));
+        hessian = hessian + (h(theta,DataTrain(k,:)') .* (1 - h(theta,DataTrain(k,:)')) .* DataTrain(k,:)' * DataTrain(k,:));
     end
     
     hessian = hessian ./m;
 
-    theta(:,i) = theta(:,i-1) - inv(hessian) * grad_log_likelihood';
+    theta = theta - inv(hessian) * grad_log_likelihood';
 end
-
-theta = theta(:,maxIterations);
