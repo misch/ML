008aa6247fdb98daa5ed5844d24fc4931eb1f6c0
= <michele_wyss@students.unibe.ch>
2013-10-25 17:59:55 +0200

    replaced loop with awesome matlab shit

--- hw2_code/GDATrain_MicheleWyss.m
+++ hw2_code/GDATrain_MicheleWyss.m
@@ -18,14 +18,19 @@ phi = sum(LabelsTrain)/m;
 mu0 = (sum(DataTrain(~LabelsTrain,:))/sum(~LabelsTrain))';
 mu1 = (sum(DataTrain(LabelsTrain,:))/sum(LabelsTrain))';
 
-%Sigma = eye(dim);
-Sigma = zeros(dim);
-for i = 1:m
-    meanVec = mu0;
-    if (LabelsTrain(i) > 0)
-        meanVec = mu1;
-    end   
-    Sigma = Sigma + ((DataTrain(i,:)'-meanVec)*((DataTrain(i,:)'-meanVec)'));   
-end
-Sigma = Sigma/m;
+% setup a matrix meanMatrix where the i-th row contains mu0 if LabelsTrain(i) = 0
+% and mu1 if LabelsTrain(i) = 1
+%
+% E.g.:					mu0(1) mu0(2) ... mu0(576)
+% Labels = [0 1 0] ===> meanMatrix =	mu1(1) mu1(2) ... mu1(576)
+%					mu0(1) mu0(2) ... mu0(576)
+%
+%
+%
+
+labelRep = repmat(LabelsTrain,[dim,1])';
+
+meansMatrix = ((repmat(mu0,[1,m])' .* ~labelRep) + (repmat(mu1,[1,m])' .* labelRep));
+
+Sigma = ((DataTrain - meansMatrix)' * (DataTrain - meansMatrix))/m;
 end
