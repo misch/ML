9bcc2c4cb841eeec18d4dd47a1efedc6452c80a9
Michele <michele_wyss@students.unibe.ch>
2013-11-27 11:00:31 +0100

    added basecode of hw4

---
+++ hw4_code/Kmeans_Dummy.m
@@ -0,0 +1,8 @@
+function [C, A] = Kmeans_Dummy(X, Cinit)
+
+% dummy
+C = Cinit;
+A = ones(1,size(X,2)); % this is not a proper assignment
+
+end
+
---
+++ hw4_code/hw4.m
@@ -0,0 +1,43 @@
+clear all;
+close all;
+clc;
+
+%% load data
+load('data1.mat');
+% load('data2.mat');
+
+K = max(labels); % number of clusters
+
+%% display data
+figure;
+scatter(X(1,:),X(2,:),'b');
+axis equal;
+title('data');
+
+colors = {'b', 'r', 'g'};
+figure;
+for j = 1:K
+    scatter( X(1,labels == j), X(2,labels == j), colors{j} );
+    hold on;
+end
+axis equal;
+title('data with ground truth cluster labelling');
+
+%% cluster with k-means
+perm = randperm(size(X,2));
+Cinit = X(:,perm(1:K));
+
+fprintf('k-means ... ');
+tic;
+[C, A] = Kmeans_Dummy(X, Cinit);
+toc;
+
+%% display clustering
+figure;
+for j = 1:K
+    scatter( X(1,A == j), X(2,A == j), colors{j} );
+    hold on;
+    scatter( C(1,j), C(2,j), 100, colors{j}, 'fill' );
+end
+axis equal;
+title('clustered data');
